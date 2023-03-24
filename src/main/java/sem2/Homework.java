731469625ace3eae9b2118e3c0e184ae97eefec3
Igor From <ig.from@yandex.ru>
2023-03-24 22:22:27 +0700

    1

---
+++ src/main/java/sem2/Homework.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Homework {
+
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(buldString(10, 'a', 'b'));
+
+        Path of = Path.of("file.txt");
+        try{
+            Files.createFile(of);
+        }catch (Exception e){
+            System.out.println( "файл уже есть");
+        }
+        Files.writeString(of, str(100, "TEXT "));
+
+    }
+
+    // 1. Дано четное число N (>0) и символы c1 и c2.
+    // Написать метод, который вернет строку длины N, которая состоит из чередующихся символов c1 и c2, начиная с c1. (6, 'a', 'b') -> "ababab"
+    static String buldString(int n, char c1, char c2){
+        String res = "";
+        for(int i = 0; i < n / 2; i++){
+            res += c1;
+            res += c2;
+        }
+        return res;
+
+    }
+
+    // 2. Создать файл с названием file.txt. Если файл уже есть, то создавать не надо Записать в него Слово "TEXT" 100 раз
+    static String str(int n, String text){
+        String res = "";
+        for(int i = 0; i < n; i++){
+            res += text;
+        }
+        return res;
+    }
+
+}
